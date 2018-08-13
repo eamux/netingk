b7a3b2af0e724caf19a400a0c49e71cd82b615ef
eamux <32377676+eamux@users.noreply.github.com>
2018-08-13 03:24:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once 'dbconnect.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home | Koding Made Simple</title>
+	<title>EAMUX EXPLORES ROUND</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" >
 	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
 </head>
@@ -20,7 +20,7 @@ include_once 'dbconnect.php';
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="index.php">Koding Made Simple</a>
+			<a class="navbar-brand" href="index.php">EAMUX MADES EASY</a>
 		</div>
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="nav navbar-nav navbar-right">
